bddac64515fa192d50bab76258f877906f7fc858
Yassine37199 <yassine.ouri7319@gmail.com>
2020-11-04 18:01:30 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import {React , Component} from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import {CardList} from './components/card-list/card-list.component';
+
+import {SearchBox} from './components/search-box/search-box.component';
+
+
+class App extends Component {
+  constructor(){
+  super();
+  this.state = {
+    cats : [],
+    searchtext : ''
+  }
+}
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({
+      cats : users
+    }))
+  }
+
+  handleChange = e => {
+    this.setState({searchtext : e.target.value })
+  }
+
+
+  render() {
+    const {cats , searchtext} = this.state;
+    const filteredCats = cats.filter(cat => 
+      cat.name.toLowerCase().includes(searchtext.toLowerCase()) )
+
+    return (
+      <div className="App">
+      <h1>Cats Rolodex</h1>
+      <SearchBox placeholder='search cat' handleChange={this.handleChange}/>
+      <CardList cats={filteredCats}/>
+      </div>
   );
+}
 }
 
 export default App;
